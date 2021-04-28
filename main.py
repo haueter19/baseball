ccff152410f24290d9e7c78ec0ac5d2422c74284
haueter19 <65046435+haueter19@users.noreply.github.com>
2021-04-27 22:13:37 -0500

    updates to sim

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ async def player_page(request: Request, pid: int):
     return templates.TemplateResponse("players.html", {"request": request, "df":df.groupby('PID').agg({'First':'first', 'Last':'first'}).reset_index(), "df2":gp.to_html(index=False), 'fname':df2.First.max(), 'lname':df2.Last.max()})
 
 @app.get("/sim", response_class=HTMLResponse)
-async def run_sims(request: Request, org: Optional[str] = 'MABL', lg: Optional[str] = '35+', innings: Optional[int] = 7, sims: Optional[int] = 100, go: Optional[int] = 0, away_lineup: Optional[str] = '2432+1781+304+876+2019+1125+750+2043+484+376', away_pitcher: Optional[int] = 484, home_lineup: Optional[str] = '579+492+391+825+1632+495+1605+1978+509', home_pitcher: Optional[int] = 825):
+async def run_sims(request: Request, org: Optional[str] = 'MABL', lg: Optional[str] = '35', innings: Optional[int] = 7, sims: Optional[int] = 1, go: Optional[int] = 0, away_lineup: Optional[str] = '2432+1781+304+876+2019+1125+750+2043+484+376', away_pitcher: Optional[int] = 484, home_lineup: Optional[str] = '579+492+391+825+1632+495+1605+1978+509', home_pitcher: Optional[int] = 825):
     if org=='MABL':
         if "+" in lg:
             pass
@@ -186,6 +186,10 @@ async def run_sims(request: Request, org: Optional[str] = 'MABL', lg: Optional[s
         away_pitcher = pit[(pit['Org']==org) & (pit['League']==lg) & (pit['Year']==2019)]['PID'].iloc[0]
         home_lineup = plyrs['PID'][:10].to_list()
         home_pitcher = pit[(pit['Org']==org) & (pit['League']==lg) & (pit['Year']==2019)]['PID'].iloc[1]
+        game_log_away = ''
+        game_log_home = ''
+        ab_results_away = ''
+        ab_results_home = ''
     else:
         import urllib.parse
         away_lineup = urllib.parse.unquote(away_lineup)
@@ -197,8 +201,8 @@ async def run_sims(request: Request, org: Optional[str] = 'MABL', lg: Optional[s
         home_lineup = home_lineup.split('+')
         for i in range(len(home_lineup)):
             home_lineup[i] = int(home_lineup[i])
-        
         prj_away = plyrs[plyrs['PID'].isin(away_lineup)].set_index('PID').reindex(away_lineup).reset_index()
+
         for i in ['1B', '2B', '3B', 'HR', 'BB', 'HBP', 'K']:
             prj_away[i+'_per_PA'] = round(prj_away[i]/prj_away['PA'],3)
         prj_home = plyrs[plyrs['PID'].isin(home_lineup)].set_index('PID').reindex(home_lineup).reset_index()
@@ -207,13 +211,12 @@ async def run_sims(request: Request, org: Optional[str] = 'MABL', lg: Optional[s
         
         home_pitcher_df = pit[(pit['PID']==home_pitcher) & (pit['Org']==org) & (pit['League']==lg) & (pit['Year']==2019)]
         away_pitcher_df = pit[(pit['PID']==away_pitcher) & (pit['Org']==org) & (pit['League']==lg) & (pit['Year']==2019)]
-        
         from sim_game import run_sim
-        rpg_away = run_sim(prj_away, home_pitcher_df, innings, sims)
-        rpg_home = run_sim(prj_home, away_pitcher_df, innings, sims)
+        rpg_away, game_log_away, ab_results_away = run_sim(prj_away, home_pitcher_df, innings, sims)
+        rpg_home, game_log_home, ab_results_home = run_sim(prj_home, away_pitcher_df, innings, sims)
         score = str(rpg_away)+' - '+str(rpg_home)
         
-    return templates.TemplateResponse("sim.html", {'request': request, 'org':org, 'lg':lg, 'score':score, 'plyrs':plyrs, 'lg_pitchers':lg_pitchers, 'away_team':away_lineup, "away_pitcher":away_pitcher, "home_team":home_lineup, "home_pitcher":home_pitcher})
+    return templates.TemplateResponse("sim.html", {'request': request, 'org':org, 'lg':lg, 'score':score, 'plyrs':plyrs, 'lg_pitchers':lg_pitchers, 'away_team':away_lineup, "away_pitcher":away_pitcher, "home_team":home_lineup, "home_pitcher":home_pitcher, 'game_log_away':game_log_away, 'game_log_home':game_log_home, 'ab_results_away':ab_results_away, 'away_proj':prj_away, 'home_proj':prj_home})
 
 @app.get("{org}/teams")
 async def teams_page(request: Request, org: Optional[str] = None, league: Optional[str] = None, year: Optional[int] = None, tm: Optional[str] = None, sort: Optional[str] = None):
--- sim_game.py
+++ sim_game.py
@@ -6,21 +6,23 @@ def at_bat_result(single_rate, double_rate, triple_rate, hr_rate, bb_rate, hbp_r
     k_rate = (k_rate+p_k)/2
     bb_rate = (bb_rate+p_bb)/2
     hbp_rate = (hbp_rate+p_hbp)/2
-    hit_rate = single_rate+double_rate+triple_rate+hr_rate
+    hit_rate = single_rate+double_rate+triple_rate+hr_rate+.0001
     single_ratio = single_rate/hit_rate
     double_ratio = double_rate/hit_rate
     triple_ratio = triple_rate/hit_rate
     hr_ratio = hr_rate/hit_rate
-    hit_rate = (hit_rate+p_h)/2
+    hit_rate = (hit_rate+p_h)/2.
     single_rate = single_ratio*hit_rate
     double_rate = double_ratio*hit_rate
     triple_rate = triple_ratio*hit_rate
     hr_rate = hr_ratio*hit_rate
     out_rate = 1-(single_rate+double_rate+triple_rate+hr_rate+bb_rate+hbp_rate+k_rate)
+    #print(bb_rate, hbp_rate, k_rate, out_rate)
+    p=[single_rate, double_rate, triple_rate, hr_rate, bb_rate, hbp_rate, k_rate, out_rate]
     return np.random.choice(['1B', '2B', '3B', 'HR', 'BB', 'HBP', 'K', 'Out'], p=[single_rate, double_rate, triple_rate, hr_rate, bb_rate, hbp_rate, k_rate, out_rate])
 
 def update_base_state(result, bases, r):
-    if result in ['1B', 'BB', 'HBP']:
+    if result in ['BB', 'HBP']:
         if bases[2] == 1: #runner on 3rd
             r += 1
             bases[2] = 0
@@ -28,6 +30,25 @@ def update_base_state(result, bases, r):
             bases[2] = 1
         if bases[0] == 1: #runner on 1st
             bases[1] = 1
+        bases[0] = 1
+    elif result=='1B':
+        if bases[2] == 1:
+            r += 1
+            bases[2] == 0
+        if bases[1] == 1:
+            if np.random.choice(['no extra base', 'extra base'], p=[.45, .55])=='no extra base':
+                bases[2] = 1
+                bases[1] = 0
+            else:
+                bases[2] = 0
+                r += 1
+                bases[1] = 0
+        if bases[0] == 1:
+            if np.random.choice(['no extra base', 'extra base'], p=[.75, .25])=='no extra base':
+                bases[1] = 1
+            else:
+                bases[2] = 1
+                bases[1] = 0
         bases[0] = 1
     elif result=='2B':
         if bases[2] == 1:
@@ -37,8 +58,13 @@ def update_base_state(result, bases, r):
             r += 1
             bases[1] = 1
         if bases[0] == 1:
-            bases[2] = 1
-            bases[0] = 0
+            if np.random.choice(['no extra base', 'extra base'], p=[.67, .33])=='no extra base':
+                bases[2] = 1
+                bases[0] = 0
+            else:
+                r += 1
+                bases[2] = 0
+                bases[0] = 0
         bases[1] = 1
     elif result=='3B':
         if bases[2] == 1:
@@ -62,16 +88,22 @@ def update_base_state(result, bases, r):
     return bases, r
 
 def sim_game(prj, tp, innings):
+    log = {}
+    ab_results = [[],[],[],[],[],[],[],[],[],[]]
     outs=0
     inn=1
     half='top'
     r = 0
     bases = [0, 0, 0]
     p = 0
+    tto = 1
     #print('Inning 1')
-    while inn<innings:
+    game_log = "<b>Inning 1</b><br>"
+    while inn<innings+1:
         result = at_bat_result(prj['1B_per_PA'].iloc[p], prj['2B_per_PA'].loc[p], prj['3B_per_PA'].iloc[p], prj['HR_per_PA'].iloc[p], prj['BB_per_PA'].iloc[p], prj['HBP_per_PA'].iloc[p], prj['K_per_PA'].iloc[p], tp['K_rate'].iloc[0], tp['BB_rate'].iloc[0], tp['HBP_rate'].iloc[0], tp['H_rate'].iloc[0])
         #print(prj['Last'].iloc[p]+':',result)
+        game_log += prj['Last'].iloc[p]+': '+result+'<br>'
+        ab_results[p-1].append(result)
         if result in ['Out', 'K']:
             outs += 1
             if outs==3:
@@ -79,6 +111,7 @@ def sim_game(prj, tp, innings):
                 inn += 1
                 bases = [0, 0, 0]
                 #print('\nInning',inn)
+                game_log += '<br><b>Inning '+str(inn)+'</b><br>'
         else:
             updates = update_base_state(result, bases, r)
             bases = updates[0]
@@ -86,13 +119,16 @@ def sim_game(prj, tp, innings):
         p += 1
         if p==10:
             p = 0
+            tto += 1
     #print(r,'runs')
-    return r
+    print(ab_results)
+    return r, game_log, ab_results
 
 def run_sim(prj, tp, innings, g):
     tot_runs = 0
     for x in range(g):
-        tot_runs += sim_game(prj, tp, innings)
+        new_runs, game_log, ab_results = sim_game(prj, tp, innings)
+        tot_runs += new_runs
         if x==g-1:
             rpg = tot_runs / g
-    return rpg
+    return rpg, game_log, ab_results
