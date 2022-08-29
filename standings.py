f7d4392de0f23468a41b8204249a761dbae6e32f
Madison-PD <85171390+Madison-PD@users.noreply.github.com>
2022-08-29 13:00:20 -0500

    Update standings.py

--- standings.py
+++ standings.py
@@ -9,12 +9,13 @@ router = APIRouter(prefix='/standings', responses={404: {"description": "Not fou
 
 @router.get("/{org}/{lg}/{yr}")
 async def standings(request: Request, org: str, lg: str, yr: int, sort: Optional[str] = 'Pct', asc: Optional[bool] = False):
-    from data_init import st
+    from data_init import st, df
     yrs = st[(st['Org']==org) & (st['League']==lg)].sort_values('Year', ascending=False)['Year'].unique().tolist()
     st = st[(st['Org']==org) & (st['League']==lg) & (st['Year']==yr)].sort_values(sort, ascending=asc)
     st['GP'] = st['W']+st['L']+st['T']
     st['Pyth'] = round(((st['RF']*st['RF']) / ((st['RF']*st['RF']) + (st['RA']*st['RA']))),3)
     st['xW'] = round(st['Pyth']*st['GP'],1)
     st['Pct'] = round(st['Pct'],3)
-    #df2 = df[(df['Org']==org) & (df['League']==lg) & (df['Year']==yr)]
+    df = df[(df['Org']==org) & (df['League']==lg) & (df['Year']==yr)].groupby('Team')['wRAAc'].sum().reset_index()
+    st = st.merge(df, on='Team', how='left').fillna(0)
     return templates.TemplateResponse("standings.html", {'request': request, 'st':st, 'org':org, 'lg':lg, 'yr':yr, 'yrs':yrs, 'df':st.fillna('').to_dict(orient='records'),})
