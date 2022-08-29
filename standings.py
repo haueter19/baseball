8960e3b0f3c978389e076cad21f77253225fe829
Madison-PD <85171390+Madison-PD@users.noreply.github.com>
2022-08-29 13:28:41 -0500

    update pitching WAR

--- standings.py
+++ standings.py
@@ -9,7 +9,7 @@ router = APIRouter(prefix='/standings', responses={404: {"description": "Not fou
 
 @router.get("/{org}/{lg}/{yr}")
 async def standings(request: Request, org: str, lg: str, yr: int, sort: Optional[str] = 'Pct', asc: Optional[bool] = False):
-    from data_init import st, df
+    from data_init import st, df, pit
     yrs = st[(st['Org']==org) & (st['League']==lg)].sort_values('Year', ascending=False)['Year'].unique().tolist()
     st = st[(st['Org']==org) & (st['League']==lg) & (st['Year']==yr)].sort_values(sort, ascending=asc)
     st['GP'] = st['W']+st['L']+st['T']
@@ -18,4 +18,8 @@ async def standings(request: Request, org: str, lg: str, yr: int, sort: Optional
     st['Pct'] = round(st['Pct'],3)
     df = df[(df['Org']==org) & (df['League']==lg) & (df['Year']==yr)].groupby('Team')['wRAAc'].sum().reset_index()
     st = st.merge(df, on='Team', how='left').fillna(0)
+    st['wRAAc'] = round(st['wRAAc'],1)
+    pit = pit[(pit['Org']==org) & (pit['League']==lg) & (pit['Year']==yr)].groupby('Team')['WAR'].sum().reset_index()
+    st = st.merge(pit, on='Team', how='left').fillna(0)
+    print(pit)
     return templates.TemplateResponse("standings.html", {'request': request, 'st':st, 'org':org, 'lg':lg, 'yr':yr, 'yrs':yrs, 'df':st.fillna('').to_dict(orient='records'),})
